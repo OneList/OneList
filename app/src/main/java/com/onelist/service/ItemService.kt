739e84898abe84d1be27f75330c27af622382426
Tony Gentile <91705356+TGentile6@users.noreply.github.com>
2023-02-22 16:56:05 -0500

    Created ItemService class and interface (no functionality)

---
+++ app/src/main/java/com/onelist/service/ItemService.kt
@@ -0,0 +1,11 @@
+package com.onelist.service
+
+interface IItemService {
+    //TODO: Create the Interface for the Item Service
+
+    //TODO: Create the method to get all the items
+}
+
+class ItemService : IItemService {
+    //TODO: Create the Service for the Item class
+}
