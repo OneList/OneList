d74005ef80d2cc972941d4958d1a4d681ab0fb3b
Rydur01 <117382074+Rydur01@users.noreply.github.com>
2023-02-23 13:42:44 -0500

    Update Item.kt
    changed id to itemID, because multiple classes have an id variable, to avoid confusion when matching later

--- app/src/main/java/com/onelist/dto/Item.kt
+++ app/src/main/java/com/onelist/dto/Item.kt
@@ -1,8 +1,8 @@
 package com.onelist.dto
 
-data class Item(var id: Int, var name: String, var categoryID: Int, var quantity : Int) {
+data class Item(var itemID: Int, var name: String, var categoryID: Int, var quantity : Int) {
 
     override fun toString(): String {
-        return "$id $name ($quantity)";
+        return "$itemID $name ($quantity)";
     }
 }
