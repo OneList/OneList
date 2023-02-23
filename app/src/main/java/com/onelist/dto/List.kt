f8e055eb85dc2bf8dae00e479a0a74e4b62a1c0a
Rydur01 <117382074+Rydur01@users.noreply.github.com>
2023-02-23 13:38:10 -0500

    Update List.kt
    made list have an id called listID

--- app/src/main/java/com/onelist/dto/List.kt
+++ app/src/main/java/com/onelist/dto/List.kt
@@ -2,7 +2,11 @@ package com.onelist.dto
 
 import kotlin.collections.List
 
+<<<<<<< Updated upstream:app/src/main/java/com/onelist/dto/List.kt
 class List<T> {
+=======
+data class ShoppingList(var listID: Int, var name: String) {
+>>>>>>> Stashed changes:app/src/main/java/com/onelist/dto/ShoppingList.kt
     //list holds Items which are strings
     private val stringList = mutableListOf<String>()
 
