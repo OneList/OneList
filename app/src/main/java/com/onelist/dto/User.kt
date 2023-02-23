66c07d01ca30ec856c4f407584c080a506237871
Rydur01 <117382074+Rydur01@users.noreply.github.com>
2023-02-23 13:40:52 -0500

    Update User.kt
    changed id to userID, because multiple classes have an id variable, to avoid confusion when matching later

---
+++ app/src/main/java/com/onelist/dto/User.kt
@@ -0,0 +1,11 @@
+package com.onelist.dto
+
+data class User(val userID: Int, var firstName: String, val lastName: String, var email: String) {
+    fun getLists(): List<ShoppingList> {
+        return emptyList()
+    }
+
+    override fun toString(): String {
+        return "$userID $firstName $lastName $email";
+    }
+}
