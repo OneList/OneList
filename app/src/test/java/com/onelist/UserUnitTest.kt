d72452fa8a8fec3ced1037ba4d5fded19f3aeba1
jtheperson <59770778+loganarnett1@users.noreply.github.com>
2023-02-24 16:38:58 -0500

    Added user toString unit test

---
+++ app/src/test/java/com/onelist/UserUnitTest.kt
@@ -0,0 +1,14 @@
+package com.onelist
+
+import com.onelist.dto.User
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class UserUnitTest {
+
+    @Test
+    fun userToString() {
+        val user = User(1, "John", "Smith", "johnsmith@test.com")
+        assertEquals("1 - John Smith (johnsmith@test.com)", user.toString())
+    }
+}
