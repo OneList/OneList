627ddfd49137b96c132fba8c9d27e38bf3e138e1
ajwhite00 <aaronjwhite00@gmail.com>
2023-02-28 13:53:41 -0500

    More specific names for Unit tests

--- app/src/test/java/com/onelist/CategoryUnitTest.kt
+++ app/src/test/java/com/onelist/CategoryUnitTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 class CategoryUnitTest {
 
     @Test
-    fun categoryToString() {
+    fun `toString returns "1 - Category Name" when categoryID = 1 & name = "Category Name"`() {
         val category = Category(1, "Category Name")
         assertEquals("1 - Category Name", category.toString())
     }
