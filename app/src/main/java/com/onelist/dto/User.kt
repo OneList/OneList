598fff9eddab68f3bcaf410c83ff1c417fa87c20
ajwhite00 <aaronjwhite00@gmail.com>
2023-02-28 16:37:57 -0500

    Added trailing commas in constructors

--- app/src/main/java/com/onelist/dto/User.kt
+++ app/src/main/java/com/onelist/dto/User.kt
@@ -1,6 +1,11 @@
 package com.onelist.dto
 
-data class User(val userID: Int, var firstName: String, val lastName: String, var email: String) {
+data class User(
+    val userID: Int,
+    var firstName: String,
+    val lastName: String,
+    var email: String,
+    ) {
 
     fun getLists(): List<ShoppingList> {
         return emptyList()
