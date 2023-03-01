8c3487994b5d9714b1ce6a8aabd6449f85720742
Mike Byrd <johnhonda41@gmail.com>
2023-02-28 20:58:39 -0500

    Updated end-point

--- app/src/main/java/com/onelist/dao/IItemDAO.kt
+++ app/src/main/java/com/onelist/dao/IItemDAO.kt
@@ -7,6 +7,6 @@ import retrofit2.http.GET
 
 interface IItemDAO {
     //TODO: change link to main branch once merged
-    @GET("/OneList/OneList/eaf1792706e3df73e5fc6a6cfa0a89db8b8d8d2c/app/ItemsJSON.txt")
+    @GET("/OneList/OneList/iByrd_CodeReview/app/ItemsJSON.txt")
     fun getAllItems() : Call<ArrayList<Item>>
 }
