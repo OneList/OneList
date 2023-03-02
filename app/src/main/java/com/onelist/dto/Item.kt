f03f1891760599aa394a681b9e3f67889f99e5f8
Subu <SUBRAMANIYAN_ANNAMALAI@hotmail.com>
2023-03-02 15:31:02 -0500

    created a kdoc for the item dto class

--- app/src/main/java/com/onelist/dto/Item.kt
+++ app/src/main/java/com/onelist/dto/Item.kt
@@ -1,7 +1,17 @@
 package com.onelist.dto
 
+/**
+ *@author Tony Gentile
+ * @property itemId the id of the item
+ * @property name the name of of the item
+ * @property categoryId the id of of the category of item
+ * @property quantity the number of of the item
+ *@constructor Creates a item class to hold item related data
+ */
 data class Item(var itemId: Int, var name: String, var categoryId: Int, var quantity : Int) {
-
+    /**
+     *@return a string representation of the item class in specific format
+     */
     override fun toString(): String {
         return "$itemId - $name ($quantity)"
     }
