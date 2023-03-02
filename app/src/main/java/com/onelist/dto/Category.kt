3bff500ea214fac9205bd684b999b2bf52e8253e
Subu <SUBRAMANIYAN_ANNAMALAI@hotmail.com>
2023-03-02 15:21:40 -0500

    created a kdoc for the category dto class

--- app/src/main/java/com/onelist/dto/Category.kt
+++ app/src/main/java/com/onelist/dto/Category.kt
@@ -1,7 +1,15 @@
 package com.onelist.dto
 
+/**
+ *@author Tony Gentile, Ryan Durham
+ * @property categoryId the id of the category
+ * @property name the names of of the categories
+ *@constructor Creates a category class to hold category data
+ */
 data class Category(val categoryId: Int, var name: String) {
-
+    /**
+     * returns a string representation of the category class in specific format
+     */
     override fun toString(): String {
         return "$categoryId - $name"
     }
