bdcdbe9c66f0f710fc3ace3d8e494e16cfaf59f9
Subu <SUBRAMANIYAN_ANNAMALAI@hotmail.com>
2023-03-02 15:53:41 -0500

    created the kdoc for the user dto class and changed variable name to match naming convention

--- app/src/main/java/com/onelist/dto/User.kt
+++ app/src/main/java/com/onelist/dto/User.kt
@@ -1,12 +1,23 @@
 package com.onelist.dto
-
-data class User(val userID: Int, var firstName: String, val lastName: String, var email: String) {
-
+/**
+ *@author Tony Gentile, Ryan durham, justin tracy
+ * @property userId the id of the user
+ * @property firstName the first name of of user
+ * @property lastName the last name of of the user
+ * @property email the email of of the user
+ *@constructor Creates a user class to hold user related data
+ */
+data class User(val userId: Int, var firstName: String, val lastName: String, var email: String) {
+    /**
+     * @returns a list
+     */
     fun getLists(): List<ShoppingList> {
         return emptyList()
     }
-
+    /**
+     *@return a string representation of the user class in specific format
+     */
     override fun toString(): String {
-        return "$userID - $firstName $lastName ($email)"
+        return "$userId - $firstName $lastName ($email)"
     }
 }
