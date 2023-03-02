50a0e1b05c44959665761bafc541464631881aa1
Subu <SUBRAMANIYAN_ANNAMALAI@hotmail.com>
2023-03-02 15:41:09 -0500

    created the kdoc for the shopping list dto class

--- app/src/main/java/com/onelist/dto/ShoppingList.kt
+++ app/src/main/java/com/onelist/dto/ShoppingList.kt
@@ -1,9 +1,20 @@
 package com.onelist.dto
 import kotlin.collections.List
 
-data class ShoppingList(var listID: Int, var name: String, var userID: Int) {
+/**
+ * subject to change after class is holding data
+ *@author  Ryan Durham
+ * @property listId the id of the shopping list
+ * @property name the name of of the shopping list
+ * @property userId the id of of the user
+ *@constructor Creates a shopping list class to hold a shopping list data
+ */
 
+data class ShoppingList(var listId: Int, var name: String, var userId: Int) {
+    /**
+     *@return a string representation of the Shopping list class in specific format
+     */
     override fun toString(): String {
-        return "$listID - $name ($userID)"
+        return "$listId - $name ($userId)"
     }
 }
