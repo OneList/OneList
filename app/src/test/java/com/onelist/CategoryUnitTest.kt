adc00238c9456065633aa91e24cba44449dbdfc4
bracko-3 <cummijb@mail.uc.edu>
2023-03-03 14:20:08 -0500

    added another unit test to test Category dto

--- app/src/test/java/com/onelist/CategoryUnitTest.kt
+++ app/src/test/java/com/onelist/CategoryUnitTest.kt
@@ -2,6 +2,7 @@ package com.onelist
 
 import com.onelist.dto.Category
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class CategoryUnitTest {
@@ -10,5 +11,12 @@ class CategoryUnitTest {
     fun `given a category dto when categoryID is 1 and name is Category Name then toString() returns "1 - Category Name"`() {
         val category = Category(1, "Category Name")
         assertEquals("1 - Category Name", category.toString())
+    }
+
+    @Test
+    fun `given a category dto when categoryID is 2 and name is Bathroom then categoryID is 2 and name is Bathroom`() {
+        var category = Category(2, "Bathroom")
+        assertTrue(category.categoryID.equals(2))
+        assertTrue(category.name.equals("Bathroom"))
     }
 }
