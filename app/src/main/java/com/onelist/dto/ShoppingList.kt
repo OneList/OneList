fd240197ecb58cfb4df9ad5229aa03865dce21f5
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2023-03-07 10:28:44 -0500

    Added KDoc for all DTO classes

--- app/src/main/java/com/onelist/dto/ShoppingList.kt
+++ app/src/main/java/com/onelist/dto/ShoppingList.kt
@@ -1,6 +1,13 @@
 package com.onelist.dto
-import kotlin.collections.List
 
+/**
+ * Class to hold shopping list data
+ *
+ * @property listId the ID of the shopping list the object references
+ * @property name the name associated with the shopping list
+ * @property userId the ID of of the user associated with the list
+ * @constructor Creates a shopping list with ID, name, and user ID
+ */
 data class ShoppingList(var listID: Int, var name: String, var userID: Int) {
 
     override fun toString(): String {
