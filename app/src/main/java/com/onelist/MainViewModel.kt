ea5d6f82ff39f6079074ed4d545ed71e86974042
arnettln <59770778+loganarnett1@users.noreply.github.com>
2023-03-24 16:01:30 -0400

    Added missing setters for other DTOs

--- app/src/main/java/com/onelist/MainViewModel.kt
+++ app/src/main/java/com/onelist/MainViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
-import com.onelist.dto.Item
+import com.onelist.dto.*
 import com.onelist.service.ItemService
 import kotlinx.coroutines.launch
 
@@ -29,7 +29,7 @@ class MainViewModel : ViewModel() {
     }
 
     fun saveItem(item: Item) {
-        val document = if (item.itemID == null || item.itemID.isEmpty()) {
+        val document = if (item.itemID.isEmpty()) {
             firestore.collection("items").document()
         } else {
             firestore.collection("items").document(item.itemID)
@@ -38,5 +38,53 @@ class MainViewModel : ViewModel() {
         val handle = document.set(item)
         handle.addOnSuccessListener { Log.d("Firebase", "Item Saved") }
         handle.addOnFailureListener { Log.d("Firebase", "Item save failed $it") }
+    }
+
+    fun saveLineItem(lineItem: LineItem) {
+        val document = if (lineItem.lineItemID.isEmpty()) {
+            firestore.collection("lineItems").document()
+        } else {
+            firestore.collection("lineItems").document(lineItem.lineItemID)
+        }
+        lineItem.lineItemID = document.id
+        val handle = document.set(lineItem)
+        handle.addOnSuccessListener { Log.d("Firebase", "Line Item Saved") }
+        handle.addOnFailureListener { Log.d("Firebase", "Line item save failed $it") }
+    }
+
+    fun saveCategory(category: Category) {
+        val document = if (category.categoryID.isEmpty()) {
+            firestore.collection("categories").document()
+        } else {
+            firestore.collection("categories").document(category.categoryID)
+        }
+        category.categoryID = document.id
+        val handle = document.set(category)
+        handle.addOnSuccessListener { Log.d("Firebase", "Category Saved") }
+        handle.addOnFailureListener { Log.d("Firebase", "Category save failed $it") }
+    }
+
+    fun saveShoppingList(shoppingList: ShoppingList) {
+        val document = if (shoppingList.listID.isEmpty()) {
+            firestore.collection("lists").document()
+        } else {
+            firestore.collection("lists").document(shoppingList.listID)
+        }
+        shoppingList.listID = document.id
+        val handle = document.set(shoppingList)
+        handle.addOnSuccessListener { Log.d("Firebase", "Shopping List Saved") }
+        handle.addOnFailureListener { Log.d("Firebase", "Shopping list save failed $it") }
+    }
+
+    fun saveUser(user: User) {
+        val document = if (user.userID.isEmpty()) {
+            firestore.collection("users").document()
+        } else {
+            firestore.collection("users").document(user.userID)
+        }
+        user.userID = document.id
+        val handle = document.set(user)
+        handle.addOnSuccessListener { Log.d("Firebase", "User Saved") }
+        handle.addOnFailureListener { Log.d("Firebase", "User save failed $it") }
     }
 }
