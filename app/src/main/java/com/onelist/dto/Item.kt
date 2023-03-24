b1ecf60f04dcda82c2906d32e8971bab98c57737
arnettln <59770778+loganarnett1@users.noreply.github.com>
2023-03-24 02:37:14 -0400

    Updated DTOs to match firestore

--- app/src/main/java/com/onelist/dto/Item.kt
+++ app/src/main/java/com/onelist/dto/Item.kt
@@ -7,13 +7,12 @@ import com.google.gson.annotations.SerializedName
  *
  * @property itemID the ID of the item the object references
  * @property name the name of the item
- * @property categoryID the ID of the category the item belongs to
- * @property quantity the quantity of the item
+ * @property categoryIDs the IDs of the categories the item belongs to
  * @constructor Creates an item with ID, name, category ID, and quantity
  */
-data class Item(@SerializedName("id") var itemID: Int, var name: String, var categoryID: Int, var quantity : Int) {
+data class Item(@SerializedName("id") var itemID: String = "", var name: String, var categoryIDs: List<String>) {
 
     override fun toString(): String {
-        return "$itemID - $name ($quantity)"
+        return "$itemID - $name"
     }
 }
