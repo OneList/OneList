9f268b5feb0d101b026a029aab417c34eb219164
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2023-03-24 17:21:53 -0400

    return

--- app/src/main/java/com/onelist/service/UserService.kt
+++ app/src/main/java/com/onelist/service/UserService.kt
@@ -1,13 +1,7 @@
 package com.onelist.service
 
-import android.content.ContentValues.TAG
-import android.util.Log
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
-import com.onelist.dao.IShoppingListDAO
+
 import com.onelist.dto.ShoppingList
-import kotlinx.coroutines.tasks.await
 
 interface IUserService {
     suspend fun fetchShoppingLists(): List<ShoppingList>?
@@ -16,5 +10,6 @@ interface IUserService {
 class UserService : IUserService {
     override suspend fun fetchShoppingLists(): List<ShoppingList>? {
       //TODO flesh this out
+        return emptyList()
     }
 }
