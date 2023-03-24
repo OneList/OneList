eec1ae0a3e00881f544c86f8323175830dbbc2fb
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2023-03-24 17:20:35 -0400

    added userservice function for lists

--- app/src/main/java/com/onelist/service/UserService.kt
+++ app/src/main/java/com/onelist/service/UserService.kt
@@ -1,4 +1,20 @@
 package com.onelist.service
 
-class UserService {
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.onelist.dao.IShoppingListDAO
+import com.onelist.dto.ShoppingList
+import kotlinx.coroutines.tasks.await
+
+interface IUserService {
+    suspend fun fetchShoppingLists(): List<ShoppingList>?
+}
+
+class UserService : IUserService {
+    override suspend fun fetchShoppingLists(): List<ShoppingList>? {
+      //TODO flesh this out
+    }
 }
