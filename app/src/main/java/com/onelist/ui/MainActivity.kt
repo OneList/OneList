0ea6491db9c4ab3a0402bb96fb6f31778116cacf
arnettln <59770778+loganarnett1@users.noreply.github.com>
2023-03-24 02:35:45 -0400

    Setup Koin

--- app/src/main/java/com/onelist/ui/MainActivity.kt
+++ app/src/main/java/com/onelist/ui/MainActivity.kt
@@ -23,10 +23,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.onelist.MainViewModel
 import com.onelist.R
+import com.onelist.dto.Item
 import com.onelist.ui.theme.OneListTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModelThing: MainViewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,208 +41,220 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun ItemName() { //Example Form
-    //TODO Will be replaced with a list of items
-    var itemName by remember { mutableStateOf("") }
-    var quantity by remember { mutableStateOf("") }
-    var category by remember { mutableStateOf("") }
-    val context = LocalContext.current
-    Column {
-        OutlinedTextField(
-            value = itemName,
-            onValueChange = { itemName = it },
-            label = { Text(stringResource(R.string.ItemName)) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = quantity,
-            onValueChange = { quantity = it },
-            label = { Text(stringResource(R.string.Quantity)) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = category,
-            onValueChange = { category = it },
-            label = { Text(stringResource(R.string.Category)) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                Toast.makeText(
-                    context,
-                    "$itemName $quantity $category",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        )
-        {
-            Text(text = "Submit")
-        }
-    }
-}
-
-@Composable
-fun ItemRow(name: String) {
-    //TODO Will be the container for a single item
-
-    //var itemName by remember { mutableStateOf("") }
-    //var quantity by remember { mutableStateOf("") }
-    //var category by remember { mutableStateOf("") }
-    //val context = LocalContext.current
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                top = 1.dp,
-                bottom = 1.dp
-            )
-    ) {
+    @Composable
+    fun ItemName() { //Example Form
+        //TODO Will be replaced with a list of items
+        var itemName by remember { mutableStateOf("") }
+        var quantity by remember { mutableStateOf("") }
+        var category by remember { mutableStateOf("") }
+        val context = LocalContext.current
         Column {
-            Text(text = name, fontSize = 20.sp, fontWeight = FontWeight.W700, modifier = Modifier.padding(2.dp)) //Name
-            Text(text = "Qty: 1", modifier = Modifier.padding(2.dp)) //Quantity
-
-        }
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.End) {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Filled.Done,
-                    contentDescription = "Mark as Purchased",
-                    tint = MaterialTheme.colors.onBackground
-                )
-            }
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    imageVector = Icons.Filled.MoreVert,
-                    contentDescription = "Edit Item",
-                    tint = MaterialTheme.colors.onBackground
-                )
+            OutlinedTextField(
+                value = itemName,
+                onValueChange = { itemName = it },
+                label = { Text(stringResource(R.string.ItemName)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = quantity,
+                onValueChange = { quantity = it },
+                label = { Text(stringResource(R.string.Quantity)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = category,
+                onValueChange = { category = it },
+                label = { Text(stringResource(R.string.Category)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    Toast.makeText(
+                        context,
+                        "$itemName $quantity $category",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            )
+            {
+                Text(text = "Submit")
             }
         }
     }
-    Divider (
-        color = MaterialTheme.colors.onBackground.copy(alpha = 0.3f),
-        modifier = Modifier
-            .height(1.dp)
-            .fillMaxHeight()
-            .fillMaxWidth()
-    )
-}
 
-@Composable
-fun CategoryHeader(name: String){
-    //TODO Header for each category
-    Divider (
-        color = MaterialTheme.colors.onBackground,
-        modifier = Modifier
-            .height(1.dp)
-            .fillMaxHeight()
-            .fillMaxWidth()
-    )
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colors.primaryVariant)
-    ){
-        Text(text = name,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.W700,
+    @Composable
+    fun ItemRow(name: String) {
+        //TODO Will be the container for a single item
+
+        //var itemName by remember { mutableStateOf("") }
+        //var quantity by remember { mutableStateOf("") }
+        //var category by remember { mutableStateOf("") }
+        //val context = LocalContext.current
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 1.dp,
+                    bottom = 1.dp
+                )
+        ) {
+            Column {
+                Text(
+                    text = name,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W700,
+                    modifier = Modifier.padding(2.dp)
+                ) //Name
+                Text(text = "Qty: 1", modifier = Modifier.padding(2.dp)) //Quantity
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(onClick = {/*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Done,
+                        contentDescription = "Mark as Purchased",
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = "Edit Item",
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
+            }
+        }
+        Divider(
+            color = MaterialTheme.colors.onBackground.copy(alpha = 0.3f),
+            modifier = Modifier
+                .height(1.dp)
+                .fillMaxHeight()
+                .fillMaxWidth()
+        )
+    }
+
+    @Composable
+    fun CategoryHeader(name: String) {
+        //TODO Header for each category
+        Divider(
             color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.padding(2.dp))
-    }
-    Divider (
-        color = MaterialTheme.colors.onBackground,
-        modifier = Modifier
-            .height(1.dp)
-            .fillMaxHeight()
-            .fillMaxWidth()
-    )
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun ItemList() { //Example List
-    //TODO Will be replaced with a list of items
-    val listItems = listOf("Bread", "Cheddar Cheese", "Apples", "Toilet Paper", "Hand Soap")
-    LazyColumn {
-        stickyHeader {
-            //TODO Make separate function for header
-            CategoryHeader("Test Header")
+            modifier = Modifier
+                .height(1.dp)
+                .fillMaxHeight()
+                .fillMaxWidth()
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colors.primaryVariant)
+        ) {
+            Text(
+                text = name,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.W700,
+                color = MaterialTheme.colors.onBackground,
+                modifier = Modifier.padding(2.dp)
+            )
         }
+        Divider(
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .height(1.dp)
+                .fillMaxHeight()
+                .fillMaxWidth()
+        )
+    }
 
-        items(listItems) { item ->
-            ItemRow(item)
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun ItemList() { //Example List
+        //TODO Will be replaced with a list of items
+        val listItems = listOf("Bread", "Cheddar Cheese", "Apples", "Toilet Paper", "Hand Soap")
+        LazyColumn {
+            stickyHeader {
+                //TODO Make separate function for header
+                CategoryHeader("Test Header")
+            }
+
+            items(listItems) { item ->
+                ItemRow(item)
+            }
         }
     }
-}
 
-@Composable
-fun ListView() { //Main Shopping List View
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "OneList") },
-                backgroundColor = MaterialTheme.colors.primary,
-                contentColor = MaterialTheme.colors.onPrimary,
+    @Composable
+    fun ListView() { //Main Shopping List View
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "OneList") },
+                    backgroundColor = MaterialTheme.colors.primary,
+                    contentColor = MaterialTheme.colors.onPrimary,
 //                navigationIcon = {
 //                    IconButton(onClick = {}) {
 //                        Icon(Icons.Filled.ArrowBack, "backIcon")
 //                    }
 //                },
-                elevation = 0.dp,
-                actions = {
-                    IconButton(onClick = {/* Do Something*/ }) {
-                        Icon(Icons.Filled.Settings, null, tint = MaterialTheme.colors.onPrimary)
+                    elevation = 0.dp,
+                    actions = {
+                        IconButton(onClick = {/* Do Something*/ }) {
+                            Icon(Icons.Filled.Settings, null, tint = MaterialTheme.colors.onPrimary)
+                        }
                     }
+                )
+
+            },
+            content = { paddingValues ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .fillMaxWidth()
+                ) {
+                    ItemList()
                 }
-            )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    backgroundColor = MaterialTheme.colors.primary,
+                    contentColor = MaterialTheme.colors.onPrimary,
+                    modifier = Modifier.size(80.dp)
+                ) {
+                    Text(
+                        text = "+",
+                        fontSize = 52.sp
+                    )
+                }
 
-        },
-        content = {paddingValues ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-            )
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .fillMaxWidth()
-            ) {
-                ItemList()
             }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ },
-                backgroundColor = MaterialTheme.colors.primary,
-                contentColor = MaterialTheme.colors.onPrimary,
-                modifier = Modifier.size(80.dp)
-            ) {
-                Text(text = "+",
-                    fontSize = 52.sp)
-            }
-
-        }
-    )
-}
-
-@Preview(showBackground = true, name = "test", device = "spec:width=411dp,height=891dp", showSystemUi = true )
-@Composable
-fun DefaultPreview() {
-    OneListTheme {
-        ListView()
+        )
     }
-}
 
-@Preview(showBackground = true, name = "test", device = "spec:width=411dp,height=891dp", showSystemUi = true )
-@Composable
-fun DarkPreview() {
-    OneListTheme(darkTheme = true) {
-        ListView()
+    @Preview(showBackground = true, name = "test", device = "spec:width=411dp,height=891dp", showSystemUi = true)
+    @Composable
+    fun DefaultPreview() {
+        OneListTheme {
+            ListView()
+        }
+    }
+
+    @Preview(showBackground = true, name = "test", device = "spec:width=411dp,height=891dp", showSystemUi = true)
+    @Composable
+    fun DarkPreview() {
+        OneListTheme(darkTheme = true) {
+            ListView()
+        }
     }
 }
