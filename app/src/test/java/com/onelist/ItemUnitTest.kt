ee830c0a94751a4a4cbb2acd1257af18cd97ef2c
loganarnett1 <loganarnett@gmail.com>
2023-04-19 20:10:10 -0400

    Updated ItemUnitTest to use mock data

--- app/src/test/java/com/onelist/ItemUnitTest.kt
+++ app/src/test/java/com/onelist/ItemUnitTest.kt
@@ -3,8 +3,17 @@ package com.onelist
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.onelist.dto.Item
 import com.onelist.service.ItemService
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 import junit.framework.TestCase.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
@@ -14,23 +23,43 @@ class ItemUnitTest {
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
 
-    lateinit var itemService: ItemService
+    @MockK
+    lateinit var mockItemService: ItemService
     var allItems: List<Item>? = ArrayList<Item>()
 
+    private val mainThreadSurrogate = newSingleThreadContext("Main Thread")
+
+    @Before
+    fun initMocksAndMainThread() {
+        MockKAnnotations.init(this)
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+
     @Test
-    fun `Given item data is available when I search for ramen then I should receive ramen`() =
+    fun `Given item data is available when I search for apple then I should receive apple`() =
         runTest {
-            givenItemServiceIsInitialized()
+            givenViewModelIsInitializedWithMockData()
             whenItemDataIsReadAndParsed()
             thenItemCollectionShouldContainApple()
         }
 
-    private fun givenItemServiceIsInitialized() {
-        itemService = ItemService()
+    private fun givenViewModelIsInitializedWithMockData() {
+        val items = ArrayList<Item>()
+        val apple = Item(name = "Apple")
+        items.add(apple)
+        val pear = Item(name = "Pear")
+        items.add(pear)
+        coEvery{mockItemService.fetchItems()} returns items
     }
 
     private suspend fun whenItemDataIsReadAndParsed() {
-        allItems = itemService.fetchItems()
+        allItems = mockItemService.fetchItems()
     }
 
     private fun thenItemCollectionShouldContainApple() {
